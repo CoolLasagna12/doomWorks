91bc3fb66189ee6a84cf1d238f1e3239371660b3
Cool Lasagna <rockingcha1r@proton.me>
2024-12-05 19:34:26 +0000

    Create player.h

---
+++ src/player.h
@@ -0,0 +1,24 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "display.h"
+#include "eadkpp.h"
+
+
+class Spaceship {
+public:
+  Player();
+  int x() const { return m_x; }
+  int y() const { return m_y; }
+  void move(int deltaX, int deltaY);
+private:
+  static constexpr int k_xLowerBound = Display::CommonHorizontalMargin;
+  static constexpr int k_xUpperBound = EADK::Screen::Width - Display::CommonHorizontalMargin;
+  static constexpr int k_yLowerBound = 3*Display::CommonVerticalMargin;
+  static constexpr int k_yUpperBound = EADK::Screen::Height - Display::CommonVerticalMargin;
+  void draw(const EADK::Color c) const;
+  int m_x;
+  int m_y;
+};
+
+#endif
