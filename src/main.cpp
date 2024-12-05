d134a969a1119524c39128ee3dcaf1e175577fa4
Cool Lasagna <rockingcha1r@proton.me>
2024-12-05 19:38:20 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,33 @@
 #include "eadkpp.h"
 #include "palette.h"
 #include "eadk_vars.h"
+#include "player.h"
 #include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
-    EADK::Display::pushRectUniform(EADK::Screen::Rect, Purple);
+    EADK::Display::pushRectUniform(EADK::Screen::Rect, Black);
 
     bool running = true;
 
+    Player player;
+    
     while (running)
     {
         EADK::Keyboard::State kbdState = EADK::Keyboard::scan();
         running = !kbdState.keyDown(EADK::Keyboard::Key::Home);
+        if (keyboardState.keyDown(EADK::Keyboard::Key::Up)) {
+            player.move(0, -Player::k_step);
+        }
+        if (keyboardState.keyDown(EADK::Keyboard::Key::Down)) {
+            player.move(0, Player::k_step);
+        }
+        if (keyboardState.keyDown(EADK::Keyboard::Key::Left)) {
+            player.move(-Player::k_step, 0);
+        }
+        if (keyboardState.keyDown(EADK::Keyboard::Key::Right)) {
+            player.move(Player::k_step, 0);
+    }
+        
     }
 }
