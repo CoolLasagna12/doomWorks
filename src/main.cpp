e3d2403712a542996213c58dc641c53f528ff3ff
Cool Lasagna <rockingcha1r@proton.me>
2024-12-05 20:16:51 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,6 @@
 #include "player.h"
 #include <stdbool.h>
 
-extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "doomWorks";
-extern const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
-
 int main(int argc, char *argv[])
 {
     EADK::Display::pushRectUniform(EADK::Screen::Rect, Black);
