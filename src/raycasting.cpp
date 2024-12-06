27f7e9b869cce5a838ef404f9dfef5773010970f
Cool Lasagna <rockingcha1r@proton.me>
2024-12-06 19:33:38 +0000

    Update raycasting.cpp

--- src/raycasting.cpp
+++ src/raycasting.cpp
@@ -1,8 +1,11 @@
 #include "raycasting.h"
 #include "palette.h"
+#include "player.h"
 
 Raycasting::Raycasting():
 
 void Raycasting::Raycast() {
-  
+  for(int x = 0; x < EADK::Screen::Width; x++) {
+    
+  }
 }
