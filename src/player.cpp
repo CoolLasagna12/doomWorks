449ee97ba7b528a0f73e7d3fdc0b9107c3687706
Cool Lasagna <rockingcha1r@proton.me>
2024-12-06 19:48:54 +0000

    Update player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -13,8 +13,9 @@ void Player::draw(const EADK::Color color) const {
   EADK::Display::pushRectUniform(EADK::Rect(p_x - 10, p_y - 10, 20, 20), color);
 }
 
-void Player::turn(int rad) {
-  direction = sin(asin(direction) + rad)
+void Player::turn(float deg) {
+  direction += rad;
+  direction %= 360
 }
 
 void Player::move(int deltaX, int deltaY) {
