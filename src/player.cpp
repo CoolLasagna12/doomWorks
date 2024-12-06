5e446ff4b4bc20fe8133db9292c9fe1caf116efc
Cool Lasagna <rockingcha1r@proton.me>
2024-12-06 19:57:44 +0000

    Update player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -14,14 +14,14 @@ void Player::draw(const EADK::Color color) const {
 }
 
 void Player::turn(float deg) {
-  direction += rad;
-  direction %= 360
+  direction += deg;
 }
 
 void Player::move(int move) {
+  tempDir = (direction * M_PI)/180;
   draw(Black);
-  p_x += deltaX;
-  p_y += deltaY;
+  p_x += move * sin(tempDir)
+  p_y -= move * cos(tempDir);
   p_x = p_x <= k_xLowerBound ? k_xLowerBound : p_x;
   p_y = p_y <= k_yLowerBound ? k_yLowerBound : p_y;
   p_x = p_x >= k_xUpperBound ? k_xUpperBound : p_x;
