41911cd4b4f2df4c78d002992b82030a43fbc62e
Cool Lasagna <rockingcha1r@proton.me>
2024-12-08 18:15:59 +0000

    Update player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -3,6 +3,7 @@
 #include "trigoApprox.h"
 #include <cmath>
 #include <stdlib.h>
+#include <string>
 
 Player::Player() :
   p_x(EADK::Screen::Width/2),
@@ -24,7 +25,10 @@ void Player::move(int move) {
   TrigoApprox trigo_obj;
   p_x += move * trigo_obj.sinus(direction, false);
   p_y -= move * trigo_obj.sinus(direction, true);
-  EADK::Display::drawString("Hello World", EADK::Point{10, 20}, false, White, Black);
+  EADK::Display::drawString("Hello World", EADK::Point{10, 0}, false, White, Black);
+  EADK::Display::drawString(std::to_string(direction), EADK::Point{10, 20}, false, White, Black);
+  EADK::Display::drawString(std::to_string(trigo_obj.sinus(direction, false), EADK::Point{10, 40}, false, White, Black);
+  EADK::Display::drawString(std::to_string(trigo_obj.sinus(direction, true), EADK::Point{10, 60}, false, White, Black);
   p_x = p_x <= k_xLowerBound ? k_xLowerBound : p_x;
   p_y = p_y <= k_yLowerBound ? k_yLowerBound : p_y;
   p_x = p_x >= k_xUpperBound ? k_xUpperBound : p_x;
