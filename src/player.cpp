2c69b3c451251b1e103f8a3161dcd7a1a90908cc
Cool Lasagna <rockingcha1r@proton.me>
2024-12-08 21:33:26 +0000

    Update player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -26,9 +26,12 @@ void Player::move(int move) {
   p_x += move * trigo_obj.sinus(direction, false);
   p_y -= move * trigo_obj.sinus(direction, true);
   EADK::Display::drawString("Hello World", EADK::Point{10, 0}, false, White, Black);
-  //EADK::Display::drawString(std::to_string(direction), EADK::Point{10, 20}, false, White, Black);
-  //EADK::Display::drawString(std::to_string(trigo_obj.sinus(direction, false), EADK::Point{10, 40}, false, White, Black);
-  //EADK::Display::drawString(std::to_string(trigo_obj.sinus(direction, true), EADK::Point{10, 60}, false, White, Black);
+  std::string text1 = std::to_string(direction);
+  std::string text2 = std::to_string(trigo_obj.sinus(direction, false));
+  std::string text3 = std::to_string(trigo_obj.sinus(direction, true));
+  EADK::Display::drawString(text1, EADK::Point{10, 20}, false, White, Black);
+  EADK::Display::drawString(text2, EADK::Point{10, 40}, false, White, Black);
+  EADK::Display::drawString(text3, EADK::Point{10, 60}, false, White, Black);
   p_x = p_x <= k_xLowerBound ? k_xLowerBound : p_x;
   p_y = p_y <= k_yLowerBound ? k_yLowerBound : p_y;
   p_x = p_x >= k_xUpperBound ? k_xUpperBound : p_x;
