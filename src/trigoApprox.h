99a1beacf1d1d3abf76b78efdfed80ea2321823d
Cool Lasagna <rockingcha1r@proton.me>
2024-12-08 17:55:21 +0000

    Update trigoApprox.h

--- src/trigoApprox.h
+++ src/trigoApprox.h
@@ -3,9 +3,9 @@
 
 class TrigoApprox {
 private:
-    static float modulo(float angle);
+    float modulo(float angle);
 public:
-    static float sinus(float x, bool cos);
+    float sinus(float x, bool cos);
 };
 
 #endif
