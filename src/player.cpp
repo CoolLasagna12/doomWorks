ea7165f9127b0b56ee6632039f3775f8d86f32e9
Cool Lasagna <rockingcha1r@proton.me>
2024-12-09 16:38:57 +0000

    Update player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -1,8 +1,6 @@
 #include "player.h"
 #include "palette.h"
 #include "trigoApprox.h"
-#include <stdlib.h>
-#include <string>
 
 Player::Player() :
   p_x(EADK::Screen::Width/2),
@@ -25,12 +23,15 @@ void Player::move(int move) {
   p_x += move * trigo_obj.sinus(direction, false);
   p_y -= move * trigo_obj.sinus(direction, true);
   EADK::Display::drawString("Hello World", EADK::Point{10, 0}, false, White, Black);
-  std::string text1 = std::to_string(direction);
-  std::string text2 = std::to_string(trigo_obj.sinus(direction, false));
-  std::string text3 = std::to_string(trigo_obj.sinus(direction, true));
-  EADK::Display::drawString(text1.c_str(), EADK::Point{10, 20}, false, White, Black);
-  EADK::Display::drawString(text2.c_str(), EADK::Point{10, 40}, false, White, Black);
-  EADK::Display::drawString(text3.c_str(), EADK::Point{10, 60}, false, White, Black);
+  char text1[32];
+  char text2[32];
+  char text3[32];
+  sprintf(text1, "%.2f", direction);
+  sprintf(text2, "%.2f", trigo_obj.sinus(direction, false));
+  sprintf(text3, "%.2f", trigo_obj.sinus(direction, true));
+  EADK::Display::drawString(text1, EADK::Point{10, 20}, false, White, Black);
+  EADK::Display::drawString(text2, EADK::Point{10, 40}, false, White, Black);
+  EADK::Display::drawString(text3, EADK::Point{10, 60}, false, White, Black);
   p_x = p_x <= k_xLowerBound ? k_xLowerBound : p_x;
   p_y = p_y <= k_yLowerBound ? k_yLowerBound : p_y;
   p_x = p_x >= k_xUpperBound ? k_xUpperBound : p_x;
