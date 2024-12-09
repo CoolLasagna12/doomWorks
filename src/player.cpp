14fb601373606183cfb8db8ae739ecd37b158d5d
Cool Lasagna <rockingcha1r@proton.me>
2024-12-09 17:06:31 +0000

    Update player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -1,8 +1,6 @@
 #include "player.h"
 #include "palette.h"
 #include "trigoApprox.h"
-#include <cstdio>
-#include <string>
 
 Player::Player() :
   p_x(EADK::Screen::Width / 2),
@@ -25,12 +23,6 @@ void Player::move(int move) {
   p_x += move * trigo_obj.sinus(direction, false);
   p_y -= move * trigo_obj.sinus(direction, true);
   EADK::Display::drawString("Hello World", EADK::Point{10, 0}, false, White, Black);
-  std::string text1 = std::to_string(direction);
-  std::string text2 = std::to_string(trigo_obj.sinus(direction, false));
-  std::string text3 = std::to_string(trigo_obj.sinus(direction, true));
-  EADK::Display::drawString(text1.c_str(), EADK::Point{10, 20}, false, White, Black);
-  EADK::Display::drawString(text2.c_str(), EADK::Point{10, 40}, false, White, Black);
-  EADK::Display::drawString(text3.c_str(), EADK::Point{10, 60}, false, White, Black);
   p_x = p_x <= k_xLowerBound ? k_xLowerBound : p_x;
   p_y = p_y <= k_yLowerBound ? k_yLowerBound : p_y;
   p_x = p_x >= k_xUpperBound ? k_xUpperBound : p_x;
