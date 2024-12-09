ccd002b851c253cbf62970f549951d2f410932ca
Cool Lasagna <rockingcha1r@proton.me>
2024-12-09 16:18:22 +0000

    Update player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -1,7 +1,7 @@
 #include "player.h"
 #include "palette.h"
 #include "trigoApprox.h"
-#include <stdlib.h>
+#include <cstdlib.h>
 #include <string>
 
 Player::Player() :
@@ -28,9 +28,12 @@ void Player::move(int move) {
   std::string text1 = std::to_string(direction);
   std::string text2 = std::to_string(trigo_obj.sinus(direction, false));
   std::string text3 = std::to_string(trigo_obj.sinus(direction, true));
-  EADK::Display::drawString(text1.c_str(), EADK::Point{10, 20}, false, White, Black);
-  EADK::Display::drawString(text2.c_str(), EADK::Point{10, 40}, false, White, Black);
-  EADK::Display::drawString(text3.c_str(), EADK::Point{10, 60}, false, White, Black);
+  char* charText1 = text1.c_str();
+  char* charText2 = text2.c_str();
+  char* charText3 = text3.c_str();
+  EADK::Display::drawString(charText1, EADK::Point{10, 20}, false, White, Black);
+  EADK::Display::drawString(charText2, EADK::Point{10, 40}, false, White, Black);
+  EADK::Display::drawString(charText3, EADK::Point{10, 60}, false, White, Black);
   p_x = p_x <= k_xLowerBound ? k_xLowerBound : p_x;
   p_y = p_y <= k_yLowerBound ? k_yLowerBound : p_y;
   p_x = p_x >= k_xUpperBound ? k_xUpperBound : p_x;
